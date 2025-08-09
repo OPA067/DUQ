bdf727c5cae393432a9c72d60c23c29ce0dc2283
OPA067 <102291315+OPA067@users.noreply.github.com>
2025-08-09 13:56:42 +0800

    Update run_MSRVTT.sh

--- script/run_MSRVTT.sh
+++ script/run_MSRVTT.sh
@@ -1,21 +1,20 @@
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 python -m torch.distributed.launch \
 --master_port 2502 \
---nproc_per_node=1 \
+--nproc_per_node=4 \
 main_retrieval.py \
 --do_train 1 \
 --workers 8 \
 --n_display 100 \
---epochs 5 \
+--epochs 10 \
 --lr 1e-4 \
 --coef_lr 1e-3 \
---batch_size 32 \
---batch_size_val 32 \
+--batch_size 128 \
+--batch_size_val 128 \
 --anno_path MSRVTT \
 --video_path MSRVTT/videos \
 --datatype msrvtt \
 --max_words 32 \
 --max_frames 12 \
 --video_framerate 1 \
---output_dir experiments/MSRVTT \
-
+--output_dir experiments/MSRVTT
